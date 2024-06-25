701ba95bfba4ad56bd31ef67414d05d05bda143a
MichaelYap808 <michaellearn808@gmail.com>
2024-06-25 22:26:42 +0700

    Initial Commit

---
+++ i am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am Rich
+//
+//  Created by Michael Sarkono on 25/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
